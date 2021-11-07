40340cd0202b5fdaa512550b0045e7c5646cfac5
Pietro Quadri <pquadri1998@gmail.com>
2021-11-07 15:14:16 +0100

    Fixed led bug

--- GROUP_02.cydsn/InterruptRoutines.c
+++ GROUP_02.cydsn/InterruptRoutines.c
@@ -17,24 +17,7 @@ extern uint8_t slaveBuffer[];
 
 CY_ISR(Custom_ISR_ADC)
 {
-    switch(FlagStatus)
-    {
-        case 0x01:
-            Pin_LED_Write(0);
-            break;
-        
-        case 0x02:
-            Pin_LED_Write(0);
-            break;
-        
-        case 0x03:
-            Pin_LED_Write(1);
-            break;
-        
-        default:
-            Pin_LED_Write(0);
-            break;
-    }
+
 }
 
 void EZI2C_ISR_ExitCallback(void)
@@ -42,6 +25,8 @@ void EZI2C_ISR_ExitCallback(void)
     if(FlagStatus != (slaveBuffer[0] & 0x03))
     {
         FlagStatus = (slaveBuffer[0] & 0x03); // Aggiorno lo stato in cui sono
+        if (FlagStatus == 0x03) Pin_LED_Write(1);
+        else Pin_LED_Write(0);
         // Ripristino condizioni di lavoro 
     }
 }
