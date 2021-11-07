57ad2249c524afe3776f19e935db0d95fdf466ed
Pietro Quadri <pquadri1998@gmail.com>
2021-11-07 14:33:42 +0100

    Added InterruptRoutines.h and .c

---
+++ GROUP_02.cydsn/InterruptRoutines.c
@@ -0,0 +1,25 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "InterruptRoutines.h"
+#include "project.h"
+
+
+CY_ISR(Custom_ISR_ADC)
+{
+    
+}
+
+void EZI2C_ISR_ExitCallback(void)
+{
+   
+}
+/* [] END OF FILE */
