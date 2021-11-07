b0c45734876c9a55f61a9b36d7eae473b9e2b6d5
Pietro Quadri <pquadri1998@gmail.com>
2021-11-07 14:47:03 +0100

    EZI2C set up in main.c

--- GROUP_02.cydsn/cyapicallbacks.h
+++ GROUP_02.cydsn/cyapicallbacks.h
@@ -15,6 +15,9 @@
 
     /*Define your macro callbacks here */
     /*For more information, refer to the Writing Code topic in the PSoC Creator Help.*/
+    #define EZI2C_ISR_EXIT_CALLBACK
+    
+    void EZI2C_ISR_ExitCallback(void);
 
     
 #endif /* CYAPICALLBACKS_H */   
